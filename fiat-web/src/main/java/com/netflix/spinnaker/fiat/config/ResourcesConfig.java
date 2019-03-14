32d5eeb72607fa1a876de56e74894d4de2342e5a
Jørgen Jervidalo <jorgen.jervidalo@schibsted.com>
2019-03-14 12:44:05 +0100

    Only enable the BuildServiceProvider if Igor is enabled

--- fiat-web/src/main/java/com/netflix/spinnaker/fiat/config/ResourcesConfig.java
+++ fiat-web/src/main/java/com/netflix/spinnaker/fiat/config/ResourcesConfig.java
@@ -30,6 +30,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -103,6 +104,7 @@ public class ResourcesConfig {
   }
 
   @Bean
+  @ConditionalOnProperty("services.igor.enabled")
   IgorApi igorApi() {
     return new RestAdapter.Builder()
         .setEndpoint(Endpoints.newFixedEndpoint(igorEndpoint))
@@ -115,6 +117,7 @@ public class ResourcesConfig {
   }
 
   @Bean
+  @ConditionalOnProperty("services.igor.enabled")
   IgorService igorService(IgorApi igorApi) {
     return new IgorService(igorApi);
   }
