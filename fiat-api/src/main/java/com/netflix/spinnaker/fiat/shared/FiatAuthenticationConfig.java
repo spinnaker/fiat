4e85f1be2969edd09cd302bbd446f3e649246cc2
Cameron Fieber <cameron@fieber.ca>
2019-04-18 22:43:31 -0700

    fix(dependencies): dependency cleanup (#377)
    * fix(dependencies): dependency cleanup
    
    * removes lots of cargo-culted dependency declarations
    * trims dependencies in fiat-core and fiat-api to minimize the
      impact consuming projects
    * guava caches -> caffeine
    
    * fix(api): cleanup FiatAuthenticationConfig WebSecurityConfigurerAdapter

--- fiat-api/src/main/java/com/netflix/spinnaker/fiat/shared/FiatAuthenticationConfig.java
+++ fiat-api/src/main/java/com/netflix/spinnaker/fiat/shared/FiatAuthenticationConfig.java
@@ -18,18 +18,14 @@ package com.netflix.spinnaker.fiat.shared;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.netflix.spectator.api.Registry;
 import com.netflix.spinnaker.config.OkHttpClientConfiguration;
-import com.netflix.spinnaker.okhttp.OkHttpMetricsInterceptor;
 import com.netflix.spinnaker.okhttp.SpinnakerRequestInterceptor;
+import com.netflix.spinnaker.retrofit.Slf4jRetrofitLogger;
 import com.squareup.okhttp.OkHttpClient;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -39,10 +35,10 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.AnonymousAuthenticationFilter;
 import org.springframework.security.web.context.SecurityContextPersistenceFilter;
 import retrofit.Endpoints;
 import retrofit.RestAdapter;
-import retrofit.client.Client;
 import retrofit.client.OkClient;
 import retrofit.converter.JacksonConverter;
 
@@ -60,8 +56,7 @@ public class FiatAuthenticationConfig {
 
   @Bean
   @ConditionalOnMissingBean(FiatService.class) // Allows for override
-  public FiatService fiatService(Registry registry,
-                                 FiatClientConfigurationProperties fiatConfigurationProperties,
+  public FiatService fiatService(FiatClientConfigurationProperties fiatConfigurationProperties,
                                  SpinnakerRequestInterceptor interceptor,
                                  OkHttpClientConfiguration okHttpClientConfiguration) {
     // New role providers break deserialization if this is not enabled.
@@ -91,40 +86,16 @@ public class FiatAuthenticationConfig {
     private final FiatStatus fiatStatus;
 
     private FiatWebSecurityConfigurerAdapter(FiatStatus fiatStatus) {
+      super(true);
       this.fiatStatus = fiatStatus;
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        /*
-         * Having `FiatAuthenticationFilter` prior to `SecurityContextPersistenceFilter` results in the
-         * `SecurityContextHolder` being overridden with a null value.
-         *
-         * The null value then causes the `AnonymousAuthenticationFilter` to inject an "anonymousUser" which when
-         * passed over the wire to fiat is promptly rejected.
-         *
-         * This behavior is triggered when `management.security.enabled` is `false`.
-         */
-        http
-            .csrf().disable()
-            .addFilterAfter(new FiatAuthenticationFilter(fiatStatus), SecurityContextPersistenceFilter.class);
-    }
-  }
-
-  private static class Slf4jRetrofitLogger implements RestAdapter.Log {
-    private final Logger logger;
-
-    Slf4jRetrofitLogger(Class type) {
-      this(LoggerFactory.getLogger(type));
-    }
-
-    Slf4jRetrofitLogger(Logger logger) {
-      this.logger = logger;
-    }
-
-    @Override
-    public void log(String message) {
-      logger.info(message);
+        http.servletApi().and()
+            .exceptionHandling().and()
+            .anonymous().and()
+            .addFilterBefore(new FiatAuthenticationFilter(fiatStatus), AnonymousAuthenticationFilter.class);
     }
   }
 }
