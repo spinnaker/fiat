spinnaker/spinnaker

commit 906ecd7724441bc4b4b4aac349306fa3eded9359

perf(fiat-roles): Cache long-running roles' synchronization process (#962)
* perf(fiat-roles): Cache long-running roles' synchronization process

With a large number of concurrently requested roles synchronizations, application uses a lot of memory and runs out of available sql connections in connection pool. We are able to assign long-running processes to Future<>, store it in in-memory cache and utilize it instead of starting new synchronization in every thread.

Change is backwards compatible, and it is not enabled by default. Property fiat.role.sync-cache.enabled governs whether to use old implementation or new one.

* perf(fiat-roles): Post-review refactoring - decreased visibility of some methods, extracted some boolean checks to variables for better readability, fixed copyright message

Co-authored-by: Mark Rampton <mark.rampton@armory.io>

fiat-roles/src/main/java/com/netflix/spinnaker/fiat/config/FiatRoleConfig.java
@@ -16,8 +16,14 @@
 
 package com.netflix.spinnaker.fiat.config;
 
+import com.netflix.spinnaker.fiat.roles.Synchronizer;
+import com.netflix.spinnaker.fiat.roles.UserRolesSyncStrategy;
+import com.netflix.spinnaker.fiat.roles.UserRolesSyncStrategy.CachedSynchronizationStrategy;
+import com.netflix.spinnaker.fiat.roles.UserRolesSyncStrategy.DefaultSynchronizationStrategy;
 import lombok.Data;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Data
@@ -26,4 +32,19 @@ import org.springframework.context.annotation.Configuration;
 public class FiatRoleConfig {
 
   private boolean orMode = false;
+
+  @Bean
+  @ConditionalOnProperty(
+      name = "fiat.role.sync-cache.enabled",
+      havingValue = "false",
+      matchIfMissing = true)
+  UserRolesSyncStrategy defaultSyncStrategy(Synchronizer synchronizer) {
+    return new DefaultSynchronizationStrategy(synchronizer);
+  }
+
+  @Bean
+  @ConditionalOnProperty(name = "fiat.role.sync-cache.enabled", havingValue = "true")
+  UserRolesSyncStrategy cachedSyncStrategy(Synchronizer synchronizer) {
+    return new CachedSynchronizationStrategy(synchronizer);
+  }
 }

fiat-roles/src/main/java/com/netflix/spinnaker/fiat/roles/Synchronizer.java
@@ -1,0 +1,240 @@
+/*
+ * Copyright 2022 Armory, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.fiat.roles;
+
+import com.netflix.spectator.api.Id;
+import com.netflix.spectator.api.Registry;
+import com.netflix.spinnaker.fiat.config.ResourceProvidersHealthIndicator;
+import com.netflix.spinnaker.fiat.config.UnrestrictedResourceConfig;
+import com.netflix.spinnaker.fiat.model.UserPermission;
+import com.netflix.spinnaker.fiat.model.resources.Role;
+import com.netflix.spinnaker.fiat.model.resources.ServiceAccount;
+import com.netflix.spinnaker.fiat.permissions.ExternalUser;
+import com.netflix.spinnaker.fiat.permissions.PermissionResolutionException;
+import com.netflix.spinnaker.fiat.permissions.PermissionsRepository;
+import com.netflix.spinnaker.fiat.permissions.PermissionsResolver;
+import com.netflix.spinnaker.fiat.providers.ProviderException;
+import com.netflix.spinnaker.fiat.providers.ResourceProvider;
+import java.util.*;
+import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.health.Status;
+import org.springframework.stereotype.Component;
+import org.springframework.util.backoff.BackOffExecution;
+import org.springframework.util.backoff.FixedBackOff;
+
+@Slf4j
+@Component
+public class Synchronizer {
+  private final ResourceProvidersHealthIndicator healthIndicator;
+  private final PermissionsResolver permissionsResolver;
+  private final PermissionsRepository permissionsRepository;
+  private final ResourceProvider<ServiceAccount> serviceAccountProvider;
+  private final Registry registry;
+  private final long retryIntervalMs;
+  private final long syncDelayTimeoutMs;
+
+  public Synchronizer(
+      ResourceProvidersHealthIndicator healthIndicator,
+      PermissionsResolver permissionsResolver,
+      PermissionsRepository permissionsRepository,
+      ResourceProvider<ServiceAccount> serviceAccountProvider,
+      Registry registry,
+      @Value("${fiat.write-mode.retry-interval-ms:10000}") long retryIntervalMs,
+      @Value("${fiat.write-mode.sync-delay-ms:600000}") long syncDelayTimeoutMs) {
+    this.retryIntervalMs = retryIntervalMs;
+    this.syncDelayTimeoutMs = syncDelayTimeoutMs;
+    this.permissionsResolver = permissionsResolver;
+    this.permissionsRepository = permissionsRepository;
+    this.serviceAccountProvider = serviceAccountProvider;
+    this.registry = registry;
+    this.healthIndicator = healthIndicator;
+  }
+
+  public long syncAndReturn(List<String> roles) {
+    FixedBackOff backoff = new FixedBackOff();
+    backoff.setInterval(this.retryIntervalMs);
+    backoff.setMaxAttempts(Math.floorDiv(this.syncDelayTimeoutMs, this.retryIntervalMs) + 1);
+    BackOffExecution backOffExec = backoff.start();
+
+    // after this point the execution will get rescheduled
+    final long timeout = System.currentTimeMillis() + this.syncDelayTimeoutMs;
+
+    if (!this.isServerHealthy()) {
+      log.warn(
+          "Server is currently UNHEALTHY. User permission role synchronization and "
+              + "resolution may not complete until this server becomes healthy again.");
+    }
+
+    // Ensure we're going to reload app and service account definitions
+    permissionsResolver.clearCache();
+
+    while (true) {
+      try {
+        Map<String, Set<Role>> combo = new HashMap<>();
+        // force a refresh of the unrestricted user in case the backing repository is empty:
+        combo.put(UnrestrictedResourceConfig.UNRESTRICTED_USERNAME, new HashSet<>());
+        Map<String, Set<Role>> temp;
+        boolean userPermissionsExists = (temp = this.getUserPermissions(roles)).isEmpty();
+        if (!userPermissionsExists) {
+          combo.putAll(temp);
+        }
+        boolean serviceAccountsExists = (temp = this.getServiceAccountsAsMap(roles)).isEmpty();
+        if (!serviceAccountsExists) {
+          combo.putAll(temp);
+        }
+
+        return this.updateUserPermissions(combo);
+      } catch (ProviderException | PermissionResolutionException ex) {
+        this.registry
+            .counter(metricName("syncFailure"), "cause", ex.getClass().getSimpleName())
+            .increment();
+        Status status = this.healthIndicator.health().getStatus();
+        long waitTime = backOffExec.nextBackOff();
+        if (waitTime == BackOffExecution.STOP || System.currentTimeMillis() > timeout) {
+          String cause = (waitTime == BackOffExecution.STOP) ? "backoff-exhausted" : "timeout";
+          registry.counter("syncAborted", "cause", cause).increment();
+          log.error("Unable to resolve service account permissions.", ex);
+          return 0;
+        }
+        String message =
+            new StringBuilder("User permission sync failed. ")
+                .append("Server status is ")
+                .append(status)
+                .append(". Trying again in ")
+                .append(waitTime)
+                .append(" ms. Cause:")
+                .append(ex.getMessage())
+                .toString();
+        if (log.isDebugEnabled()) {
+          log.debug(message, ex);
+        } else {
+          log.warn(message);
+        }
+
+        try {
+          Thread.sleep(waitTime);
+        } catch (InterruptedException ignored) {
+        }
+      } finally {
+        this.isServerHealthy();
+      }
+    }
+  }
+
+  private boolean isServerHealthy() {
+    return healthIndicator.health().getStatus() == Status.UP;
+  }
+
+  private Map<String, Set<Role>> getUserPermissions(List<String> roles) {
+    if (roles == null || roles.isEmpty()) {
+      return permissionsRepository.getAllById();
+    } else {
+      return permissionsRepository.getAllByRoles(roles);
+    }
+  }
+
+  private long updateUserPermissions(Map<String, Set<Role>> rolesById) {
+    if (rolesById.remove(UnrestrictedResourceConfig.UNRESTRICTED_USERNAME) != null) {
+      timeIt(
+          "syncAnonymous",
+          () -> {
+            permissionsRepository.put(permissionsResolver.resolveUnrestrictedUser());
+            log.info("Synced anonymous user role.");
+          });
+    }
+
+    List<ExternalUser> extUsers =
+        rolesById.entrySet().stream()
+            .map(
+                entry ->
+                    new ExternalUser()
+                        .setId(entry.getKey())
+                        .setExternalRoles(
+                            entry.getValue().stream()
+                                .filter(role -> role.getSource() == Role.Source.EXTERNAL)
+                                .collect(Collectors.toList())))
+            .collect(Collectors.toList());
+
+    if (extUsers.isEmpty()) {
+      log.info("Found no non-anonymous user roles to sync.");
+      return 0;
+    }
+
+    long count =
+        timeIt(
+            "syncUsers",
+            () -> {
+              Map<String, UserPermission> values = permissionsResolver.resolve(extUsers);
+              permissionsRepository.putAllById(values);
+              return values.size();
+            });
+    log.info("Synced {} non-anonymous user roles.", count);
+    return count;
+  }
+
+  private Map<String, Set<Role>> getServiceAccountsAsMap(List<String> roles) {
+    List<UserPermission> allServiceAccounts =
+        serviceAccountProvider.getAll().stream()
+            .map(ServiceAccount::toUserPermission)
+            .collect(Collectors.toList());
+    if (roles == null || roles.isEmpty()) {
+      return allServiceAccounts.stream()
+          .collect(Collectors.toMap(UserPermission::getId, UserPermission::getRoles));
+    } else {
+      return allServiceAccounts.stream()
+          .filter(p -> p.getRoles().stream().map(Role::getName).anyMatch(roles::contains))
+          .collect(Collectors.toMap(UserPermission::getId, UserPermission::getRoles));
+    }
+  }
+
+  private void timeIt(String timerName, Runnable theThing) {
+    Callable<Object> c =
+        () -> {
+          theThing.run();
+          return null;
+        };
+    timeIt(timerName, c);
+  }
+
+  private <T> T timeIt(String timerName, Callable<T> theThing) {
+    long startTime = System.nanoTime();
+    String cause = null;
+    try {
+      return theThing.call();
+    } catch (RuntimeException re) {
+      cause = re.getClass().getSimpleName();
+      throw re;
+    } catch (Exception ex) {
+      cause = ex.getClass().getSimpleName();
+      throw new RuntimeException(ex);
+    } finally {
+      boolean success = cause == null;
+      Id timer = registry.createId(metricName(timerName)).withTag("success", success);
+      registry
+          .timer(success ? timer : timer.withTag("cause", cause))
+          .record(System.nanoTime() - startTime, TimeUnit.NANOSECONDS);
+    }
+  }
+
+  private static String metricName(String name) {
+    return "fiat.userRoles." + name;
+  }
+}

fiat-roles/src/main/java/com/netflix/spinnaker/fiat/roles/CallableCache.java
@@ -1,0 +1,81 @@
+/*
+ * Copyright 2022 Armory, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.fiat.roles;
+
+import java.util.concurrent.*;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.function.Predicate;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class CallableCache<Key, Result> {
+
+  private static final Predicate<Future> isExecutionInProgress =
+      future -> !future.isDone() && !future.isCancelled();
+
+  private final ExecutorService executor;
+  private final ConcurrentHashMap<Key, Future<Result>> cache;
+  private final Lock lock;
+
+  public CallableCache() {
+    this.executor = Executors.newCachedThreadPool();
+    this.cache = new ConcurrentHashMap<>();
+    this.lock = new ReentrantLock();
+  }
+
+  Future<Result> runAndGetResult(Key key, Callable<Result> callable) {
+    try {
+      lock.lock();
+      if (isPresent(key)) {
+        var futureResultFromCache = cache.get(key);
+        if (isExecutionInProgress.test(futureResultFromCache)) {
+          log.info(
+              "There's running callable in cache associated with the key. Reusing that execution");
+          return futureResultFromCache;
+        }
+      }
+      log.debug("Starting new callable execution...");
+      return cacheAndRun(key, callable);
+    } finally {
+      lock.unlock();
+    }
+  }
+
+  void clear(Key key) {
+    try {
+      lock.lock();
+      var future = cache.get(key);
+      if (future != null && (future.isDone() || future.isCancelled())) {
+        cache.remove(key);
+        log.debug("Removing element from cache identified by key: " + key);
+      }
+    } finally {
+      lock.unlock();
+    }
+  }
+
+  private boolean isPresent(Key key) {
+    return this.cache.containsKey(key);
+  }
+
+  private Future<Result> cacheAndRun(Key key, Callable<Result> callable) {
+    var future = executor.submit(callable);
+    this.cache.put(key, future);
+    return future;
+  }
+}

fiat-roles/src/main/java/com/netflix/spinnaker/fiat/roles/UserRolesSyncStrategy.java
@@ -1,0 +1,76 @@
+/*
+ * Copyright 2022 Armory, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.fiat.roles;
+
+import java.util.List;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@FunctionalInterface
+public interface UserRolesSyncStrategy {
+  long syncAndReturn(List<String> roles);
+
+  @RequiredArgsConstructor
+  class DefaultSynchronizationStrategy implements UserRolesSyncStrategy {
+
+    private final Synchronizer synchronizer;
+
+    @Override
+    public long syncAndReturn(List<String> roles) {
+      return this.synchronizer.syncAndReturn(roles);
+    }
+  }
+
+  @Slf4j
+  class CachedSynchronizationStrategy implements UserRolesSyncStrategy {
+
+    private final Synchronizer synchronizer;
+    private final CallableCache<List<String>, Long> callableCache;
+
+    public CachedSynchronizationStrategy(@NonNull Synchronizer synchronizer) {
+      this.synchronizer = synchronizer;
+      this.callableCache = new CallableCache<>();
+    }
+
+    public CachedSynchronizationStrategy(
+        @NonNull Synchronizer synchronizer, @NonNull CallableCache<List<String>, Long> cache) {
+      this.synchronizer = synchronizer;
+      this.callableCache = cache;
+    }
+
+    @Override
+    public long syncAndReturn(List<String> roles) {
+      try {
+        return this.callableCache
+            .runAndGetResult(roles, () -> this.synchronizer.syncAndReturn(roles))
+            .get();
+      } catch (Exception e) {
+        log.error(e.getMessage());
+        throw new RolesSynchronizationException();
+      } finally {
+        this.callableCache.clear(roles);
+      }
+    }
+  }
+
+  final class RolesSynchronizationException extends RuntimeException {
+    RolesSynchronizationException() {
+      super("Unexpected exception occurred while running roles synchronization");
+    }
+  }
+}

spinnaker/fiat

commit fb637a7863f633a1c41ba9c346f3a321ae0abba4

fix(fiat-roles): Fiat fails to start if write mode is disabled
Spring refuses to start due to a missing bean when write mode is disabled. The roles sync should not be enabled at all when write mode is disabled, so this fix disables both sync strategies and the Synchronizer class if read mode is disabled.
Fixes bug introduced in https://github.com/spinnaker/fiat/pull/962.

Error message:

```
***************************
APPLICATION FAILED TO START
***************************
Description:
Parameter 0 of constructor in com.netflix.spinnaker.fiat.roles.Synchronizer required a bean of type 'com.netflix.spinnaker.fiat.config.ResourceProvidersHealthIndicato>
Action:
Consider defining a bean of type 'com.netflix.spinnaker.fiat.config.ResourceProvidersHealthIndicator' in your configuration.
```

fiat-roles/src/main/java/com/netflix/spinnaker/fiat/config/FiatRoleConfig.java
@@ -21,6 +21,7 @@ import com.netflix.spinnaker.fiat.roles.UserRolesSyncStrategy;
 import com.netflix.spinnaker.fiat.roles.UserRolesSyncStrategy.CachedSynchronizationStrategy;
 import com.netflix.spinnaker.fiat.roles.UserRolesSyncStrategy.DefaultSynchronizationStrategy;
 import lombok.Data;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -38,12 +39,14 @@ public class FiatRoleConfig {
       name = "fiat.role.sync-cache.enabled",
       havingValue = "false",
       matchIfMissing = true)
+  @ConditionalOnExpression("${fiat.write-mode.enabled:true}")
   UserRolesSyncStrategy defaultSyncStrategy(Synchronizer synchronizer) {
     return new DefaultSynchronizationStrategy(synchronizer);
   }
 
   @Bean
   @ConditionalOnProperty(name = "fiat.role.sync-cache.enabled", havingValue = "true")
+  @ConditionalOnExpression("${fiat.write-mode.enabled:true}")
   UserRolesSyncStrategy cachedSyncStrategy(Synchronizer synchronizer) {
     return new CachedSynchronizationStrategy(synchronizer);
   }

fiat-roles/src/main/java/com/netflix/spinnaker/fiat/roles/Synchronizer.java
@@ -29,19 +29,26 @@ import com.netflix.spinnaker.fiat.permissions.PermissionsRepository;
 import com.netflix.spinnaker.fiat.permissions.PermissionsResolver;
 import com.netflix.spinnaker.fiat.providers.ProviderException;
 import com.netflix.spinnaker.fiat.providers.ResourceProvider;
-import java.util.*;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.Callable;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.health.Status;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.stereotype.Component;
 import org.springframework.util.backoff.BackOffExecution;
 import org.springframework.util.backoff.FixedBackOff;
 
 @Slf4j
 @Component
+@ConditionalOnExpression("${fiat.write-mode.enabled:true}")
 public class Synchronizer {
   private final ResourceProvidersHealthIndicator healthIndicator;
   private final PermissionsResolver permissionsResolver;